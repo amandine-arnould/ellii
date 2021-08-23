fc44add6c6b41b04d21a7574a7d24601756716f1
amandine arnould <amandine.arnould90@gmail.com>
2021-08-23 18:22:39 +0200

    Added policy for bookings

---
+++ app/policies/booking_policy.rb
@@ -0,0 +1,27 @@
+class BookingPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    user.status == "admin" || user.status == "senior"  || user.status == "relative"
+  end
+
+  def show?
+    user.status == "admin" || user.status == "senior"  || user.status == "relative"
+  end
+
+  def new?
+    user.status == "admin" || user.status == "senior"  || user.status == "relative"
+  end
+
+  def create?
+    user.status == "admin" || user.status == "senior"  || user.status == "relative"
+  end
+
+  def destroy?
+    user.status == "admin" || user.status == "senior"  || user.status == "relative"
+  end
+end
