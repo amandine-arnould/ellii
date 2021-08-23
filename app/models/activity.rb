0de40a581e9117402529b2b80e58713f939dc2e1
amandine arnould <amandine.arnould90@gmail.com>
2021-08-23 15:47:28 +0200

    Init controllers

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,6 @@
 class Activity < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   validates :title, presence: true
   validates :description, presence: true, length: { minimum: 250 }
   validates :duration, presence: true
