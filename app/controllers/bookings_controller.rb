c85ac7f7d04ea945e49944967f07ec275daeecab
Daniela Mock Zubia <mockzubia@gmail.com>
2021-08-24 11:24:48 +0200

    booking controller and createdash file

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,30 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    authorize @booking
+  end
+
+  def create
+    @booking = Booking.new
+    @session = Session.find(params[:activity_id])
+    @booking.user = current_user
+    @booking.session = @session
+
+    authorize @booking
+
+    if @booking.save
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+
+    authorize @booking
+    redirect_back(fallback_location: dashboard_path)
+  end
+
 end
