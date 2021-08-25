0033e660b55a944494c4922fc3a33c17fc79ad6a
amandine arnould <amandine.arnould90@gmail.com>
2021-08-25 18:10:26 +0200

    Fixed js bug undefined - Removed js function - Addedmood partial

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,13 +30,11 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 
 import { triggerActivityAddress } from "../components/trigger_activity_address";
-import { moodSmiley } from "../components/mood_smiley";
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   triggerActivityAddress();
-  moodSmiley();
 });
 
 import "controllers"
