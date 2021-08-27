2be31a166ca5540dba35c2a2a39a2f0149094d85
Daniela Mock Zubia <mockzubia@gmail.com>
2021-08-27 13:20:21 +0200

    changed dates in dash to french

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,8 @@ class PagesController < ApplicationController
   end
 
   def dashboard
+    @month = { 4=>"Avril", 9=>"Septembre", 6=>"Juin", 3=>"Mars", 2=>"Février", 1=>"Janvier", 11=>"Novembre", 8=>"Août", 10=>"Octobre", 7=>"Juillet", 12=>"Décembre", 5=>"Mai"}
+
     @user_info = current_user
     @user_bookings = Booking.where(user: current_user)
   end
