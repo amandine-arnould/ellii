fdd5d83cfd2aa1702e8fb2cd4b56ae4f714bc76e
Daniela Mock Zubia <mockzubia@gmail.com>
2021-08-27 15:45:46 +0200

    fixed dates on show and dash

--- app/models/session.rb
+++ app/models/session.rb
@@ -15,6 +15,6 @@ class Session < ApplicationRecord
 
   def name
     months = { 4=>"Avril", 9=>"Septembre", 6=>"Juin", 3=>"Mars", 2=>"Février", 1=>"Janvier", 11=>"Novembre", 8=>"Août", 10=>"Octobre", 7=>"Juillet", 12=>"Décembre", 5=>"Mai"}
-    start_at.strftime("%d").to_s + " " + months[start_at.month] + ", " + start_at.strftime("%Y").to_s + " à " + start_at.strftime("%H").to_s
+    start_at.strftime("%d").to_s + " " + months[start_at.month] + ", " + start_at.strftime("%Y").to_s + " à " + start_at.strftime("%H").to_s + "h" + start_at.strftime("%M").to_s
   end
 end
