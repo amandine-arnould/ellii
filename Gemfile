66a08031cf2764789264e1f6fffe7813a5d447bc
Thibaut Baissac <tbaissac@gmail.com>
2021-08-28 15:34:48 +0200

    Active admin style + structure

--- Gemfile
+++ Gemfile
@@ -39,8 +39,12 @@ gem 'pundit'
 gem 'geocoder'
 gem "abyme"
 gem "twilio-ruby"
+
+# Active admin
 gem "activeadmin", github: "activeadmin/activeadmin"
 gem "inherited_resources", github: "activeadmin/inherited_resources"
+gem "active_admin_flat_skin"
+gem "font-awesome-rails"
 
 group :development, :test do  gem 'pry-byebug'
   gem 'pry-rails'
--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,22 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel "Recent Users" do
+          table_for User.order("id desc").limit(10).each do |_user|
+            column(:email) { |user| link_to(user.email, admin_user_path(user)) }
+          end
+        end
+      end
+
+      column do
+        panel "Recent Activities" do
+          table_for Activity.order("id desc").limit(10).each do |_activity|
+            column(:title) { |activity| link_to(activity.title, admin_user_path(activity)) }
+          end
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
