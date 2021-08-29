ededf6e4f2512401af4993f3523d6863a120b72c
Thibaut Baissac <tbaissac@gmail.com>
2021-08-29 19:29:50 +0200

    Added balades to senior dashboard and move ephad to admin

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ require "json"
 require "open-uri"
 
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
     @mood = Mood.new
@@ -10,10 +10,15 @@ class PagesController < ApplicationController
   end
 
   def dashboard
-    @month = { 4=>"Avril", 9=>"Septembre", 6=>"Juin", 3=>"Mars", 2=>"Février", 1=>"Janvier", 11=>"Novembre", 8=>"Août", 10=>"Octobre", 7=>"Juillet", 12=>"Décembre", 5=>"Mai"}
+    @month = { 4 => "Avril", 9 => "Septembre", 6 => "Juin", 3 => "Mars", 2 => "Février", 1 => "Janvier", 11 => "Novembre", 8 => "Août", 10 => "Octobre", 7 => "Juillet", 12 => "Décembre", 5 => "Mai" }
 
     @user_info = current_user
     @user_bookings = Booking.where(user: current_user)
+
+    # BALADES
+    url = "https://opendata.paris.fr/api/records/1.0/search/?dataset=paris-autrement-balades-dans-les-arrondissements-peripheriques-parcours"
+    balades_serialized = URI.open(url).read
+    @balades = JSON.parse(balades_serialized)
 
     #EPHAD
     url = "https://opendata.paris.fr/api/records/1.0/search/?dataset=liste-des-ehpad"
@@ -21,5 +26,4 @@ class PagesController < ApplicationController
     @ephad = JSON.parse(ephad_serialized)
     @image_url = "https://opendata.paris.fr/api/v2/catalog/datasets/liste-des-ehpad/files/"
   end
-
 end
