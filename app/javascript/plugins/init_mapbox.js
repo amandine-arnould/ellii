690215ce6385e1c2b73ab85eeafe8582cc64fb16
Arthur Herminie <arthur.herminie@gmail.com>
2021-09-01 12:44:16 +0200

    added mapbox itinerary

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -4,19 +4,87 @@ import "mapbox-gl/dist/mapbox-gl.css";
 const initMapbox = () => {
   const mapElement = document.getElementById("map");
 
-  const fitMapToMarkers = (map, marker) => {
+  // const fitMapToMarkers = (map, marker) => {
+  //   const bounds = new mapboxgl.LngLatBounds();
+  //   bounds.extend([marker.lng, marker.lat]);
+  //   map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  // };
+
+  const fitMapToMarkers = (map, markers) => {
     const bounds = new mapboxgl.LngLatBounds();
-    bounds.extend([marker.lng, marker.lat]);
+    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
     map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
   };
 
   const addMarkersToMap = (map, marker) => {
     const popup = new mapboxgl.Popup().setHTML(marker.info_window); // add this
 
-    new mapboxgl.Marker()
+    const element = document.createElement('div');
+    element.className = 'marker';
+    element.style.backgroundImage = `url('${marker.image_url}')`;
+    element.style.backgroundSize = 'contain';
+    element.style.backgroundRepeat = 'no-repeat';
+    element.style.padding = '0';
+    element.style.width = '30px';
+    element.style.height = '30px';
+
+    new mapboxgl.Marker(element)
       .setLngLat([marker.lng, marker.lat])
       .setPopup(popup) // add this
       .addTo(map);
+  };
+
+  const addUserMarkersToMap = (map, userMarker) => {
+    const element = document.createElement('div');
+    element.className = 'user-marker';
+    element.style.backgroundImage = `url('${userMarker.image_url}')`;
+    element.style.backgroundSize = 'contain';
+    element.style.backgroundRepeat = 'no-repeat';
+    element.style.width = '30px';
+    element.style.height = '30px';
+
+    new mapboxgl.Marker(element)
+      .setLngLat([userMarker.lng, userMarker.lat])
+      .addTo(map);
+  };
+
+  async function getRoute(map, start, end) {
+    // make directions request using cycling profile
+    const query = await fetch(
+      `https://api.mapbox.com/directions/v5/mapbox/walking/${start[0]},${start[1]};${end[0]},${end[1]}?geometries=geojson&access_token=${mapboxgl.accessToken}`,
+      { method: 'GET' }
+    );
+
+    const json = await query.json();
+    const data = json.routes[0];
+    const route = data.geometry.coordinates;
+
+    const geojson = {
+      'type': 'Feature',
+      'properties': {},
+      'geometry': {
+        'type': 'LineString',
+        'coordinates': route
+      }
+    };
+
+    map.addLayer({
+      'id': 'route',
+      'type': 'line',
+      'source': {
+        'type': 'geojson',
+        'data': geojson
+      },
+      'layout': {
+        'line-join': 'round',
+        'line-cap': 'round'
+      },
+      'paint': {
+        'line-color': '#6447F9',
+        'line-width': 5,
+        'line-opacity': 0.5
+      }
+    });
   };
 
   if (mapElement) {
@@ -27,12 +95,20 @@ const initMapbox = () => {
       style: 'mapbox://styles/mapbox/streets-v10'
     });
 
-    const marker = JSON.parse(mapElement.dataset.marker);
-    new mapboxgl.Marker().setLngLat([marker.lng, marker.lat]).addTo(map);
+    const marker     = JSON.parse(mapElement.dataset.marker);
+    const userMarker = JSON.parse(mapElement.dataset.userMarker);
 
-    fitMapToMarkers(map, marker);
+    const start = [userMarker.lng, userMarker.lat];
+    const end   = [marker.lng, marker.lat];
+
+    fitMapToMarkers(map, [marker, userMarker]);
     addMarkersToMap(map, marker);
-  }
-};
+    addUserMarkersToMap(map, userMarker);
+
+    map.on('load', () => {
+      getRoute(map, start, end);
+    });
+  };
+}
 
 export { initMapbox };
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_30_101250) do
+ActiveRecord::Schema.define(version: 2021_09_01_080456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -120,6 +120,8 @@ ActiveRecord::Schema.define(version: 2021_08_30_101250) do
     t.string "phone_number"
     t.string "address"
     t.boolean "admin", default: false, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
